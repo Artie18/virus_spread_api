fe0d33ea9382e675764c8e4e7c07ff817a97ae90
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-03-27 23:51:59 +0300

    Adde human-virus index (Since we are using nosql...but I love no sql...but sometimes it's shee..p)

---
+++ models/human-virus-index.js
@@ -0,0 +1,7 @@
+var HumanVirusSchema = {
+  humanId: '', // id of sick human
+  virusId: '', // id of a virus
+  time: new Date(), // time when he got sick
+  lat: '', // lat of loc where he got sick
+  ln: ''   // ln of loc where he got sick
+}
