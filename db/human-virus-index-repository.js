68379cd27930f3895098313d47880c21047b8eb8
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-03-28 06:39:48 +0300

    Human Virus Index data flow bug fix

--- db/human-virus-index-repository.js
+++ db/human-virus-index-repository.js
@@ -3,14 +3,12 @@ var cass = require('../lib/cassandra-api').db;
 exports.db = function () {
   return {
     create: function (hv, cb) {
-      cass.exec("INSERT INTO virus_index_schema" +
-        "(id, humanId, virusId, lat, ln, time) " +
-        "VALUES (" +
-        hv.id + "," +
-        hv.humanId + "," +
-        hv.virusId + "," +
-        "'" + hv.lat + "'," +
-        "'" + hv.time + "')", cb);
+      params = [hv.id, hv.humanId, hv.virusId, hv.lat, hv.ln, hv.time]
+      console.log(params)
+      cass.exec_with_params("INSERT INTO human_virus_index " +
+        "(id, humaId, virusId, lat, ln, time) " +
+        "VALUES (?, ?, ?, ?, ?, ?);",
+        params, cb);
     }
   }
 }
--- models/human-virus-index.js
+++ models/human-virus-index.js
@@ -1,28 +1,29 @@
 var rep = require('../db/human-virus-index-repository').db();
 var uuid = require('node-uuid');
 
-var HumanVirusSchema = {
-  id: '', // id
-  humanId: '', // id of sick human
-  virusId: '', // id of a virus
-  time: new Date(), // time when he got sick
-  lat: '', // lat of loc where he got sick
-  ln: ''   // ln of loc where he got sick
+function humanVirusSchema() {
+  return {
+    id: '', // id
+    humanId: '', // id of sick human
+    virusId: '', // id of a virus
+    time: new Date(), // time when he got sick
+    lat: '', // lat of loc where he got sick
+    ln: ''   // ln of loc where he got sick
+  }
 }
 
 exports.humanVirusIndex = function () {
   return {
     create: function (q, cb) {
-      hvSchema = new HumanVirusSchema();
-      hvSchema = {
+      var hvSchema = {
         id: uuid.v4(),
-        humanId: q.deviceID,
+        humanId: q.deviceId,
         virusId: q.type,
         lat: q.location.lat,
         ln: q.location.ln,
         time: q.time
       };
-      rep.create(hSchema, cb);
+      rep.create(hvSchema, cb);
     },
   }
 
