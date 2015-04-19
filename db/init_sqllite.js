ab288bdc2609795569bfb0364203cac41f34d79b
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-04-19 17:29:59 +0300

    Added waterfall for sqllite db init

--- db/init_sqllite.js
+++ db/init_sqllite.js
@@ -1,57 +1,73 @@
 var db = require('../lib/sqlite-api').db;
+var async = require('async');
 
-db.exec('CREATE TABLE IF NOT EXISTS humans (' +
-  'id uuid PRIMARY KEY, ' +
-  'name text, ' +
-  'sickWith text, ' +
-  'gender text, ' +
-  'age int )', function () { console.log('Created humans')})
-
-db.exec('CREATE TABLE IF NOT EXISTS viruses (' +
-  'title text PRIMARY KEY, ' +
-  'description text)', function () { console.log('Created Viruses')})
-
-db.exec('CREATE TABLE IF NOT EXISTS kisses (' +
-  'id uuid, ' +
-  'lt text, ' +
-  'ln text, ' +
-  'time timestamp, ' +
-  'kissedBy uuid, ' +
-  'kissedOn uuid, ' +
-  'PRIMARY KEY (kissedBy, kissedOn, id) )', function () { console.log('Created Kisses')})
-
-db.exec('CREATE TABLE IF NOT EXISTS points (' +
-  'id uuid PRIMARY KEY, ' +
-  'lt text, ' +
-  'ln text, ' +
-  'visitedBy uuid)', function () { console.log('Created Points')})
-
-db.exec('CREATE TABLE IF NOT EXISTS markers (' +
-  'id uuid PRIMARY KEY, ' +
-  'lt text, ' +
-  'ln text)', function () { console.log('Created Points')})
-
-db.exec('CREATE TABLE IF NOT EXISTS human_virus_index (' +
-  'id uuid PRIMARY KEY, ' +
-  'humaId uuid, ' +
-  'virusId text, ' +
-  'time timestamp, ' +
-  'lat text, ' +
-  'ln text)', function () { console.log('Created Human Virus Index')});
-
-db.exec('CREATE TABLE IF NOT EXISTS meeting_count (' +
-  'kissedOn uuid, ' +
-  'kissedBy uuid, ' +
-  'count int, ' +
-  'PRIMARY KEY (kissedOn, kissedBy))', function () { });
-
-db.exec('ALTER TABLE humans ADD COLUMN infectedBy uuid', function (err,res) {
-  console.log('Errors are: ' + err);
-  console.log('Res are: ' + res);
-});
-db.exec('CREATE INDEX kisses_kissedOn on kisses(kissedOn)', function () {});
-db.exec('CREATE INDEX kisses_kissedBy on kisses(kissedBy)', function () {});
-
-db.close();
-
-//create index user_accounts_country on user_accounts(country);
+async.waterfall([
+  function(cb) {
+    db.exec('CREATE TABLE IF NOT EXISTS humans (' +
+      'id uuid PRIMARY KEY, ' +
+      'name text, ' +
+      'sickWith text, ' +
+      'gender text, ' +
+      'age int )', function () { cb(); });
+  },
+  function (cb) {
+    db.exec('CREATE TABLE IF NOT EXISTS viruses (' +
+      'title text PRIMARY KEY, ' +
+      'description text)', function () { console.log('Created Viruses'); cb();})
+  },
+  function (cb) {
+    db.exec('CREATE TABLE IF NOT EXISTS kisses (' +
+      'id uuid, ' +
+      'lt text, ' +
+      'ln text, ' +
+      'time timestamp, ' +
+      'kissedBy uuid, ' +
+      'kissedOn uuid, ' +
+      'PRIMARY KEY (kissedBy, kissedOn, id) )', function () { console.log('Created Kisses'); cb();})
+  },
+  function (cb) {
+    db.exec('CREATE TABLE IF NOT EXISTS points (' +
+      'id uuid PRIMARY KEY, ' +
+      'lt text, ' +
+      'ln text, ' +
+      'visitedBy uuid)', function () { console.log('Created Points'); cb(); })
+  },
+  function (cb) {
+    db.exec('CREATE TABLE IF NOT EXISTS markers (' +
+      'id uuid PRIMARY KEY, ' +
+      'lt text, ' +
+      'ln text)', function () { console.log('Created Points'); cb(); })
+  },
+  function (cb) {
+    db.exec('CREATE TABLE IF NOT EXISTS human_virus_index (' +
+      'id uuid PRIMARY KEY, ' +
+      'humaId uuid, ' +
+      'virusId text, ' +
+      'time timestamp, ' +
+      'lat text, ' +
+      'ln text)', function () { console.log('Created Human Virus Index'); cb(); });
+  },
+  function (cb) {
+    db.exec('CREATE TABLE IF NOT EXISTS meeting_count (' +
+      'kissedOn uuid, ' +
+      'kissedBy uuid, ' +
+      'count int, ' +
+      'PRIMARY KEY (kissedOn, kissedBy))', function () { cb(); });
+  },
+  function (cb) {
+    db.exec('ALTER TABLE humans ADD COLUMN infectedBy uuid', function (err,res) {
+      console.log('Errors are: ' + err);
+      console.log('Res are: ' + res);
+      cb();
+    });
+  },
+  function (cb) {
+    db.exec('CREATE INDEX kisses_kissedOn on kisses(kissedOn)', function () { cb(); });
+  },
+  function (cb) {
+    db.exec('CREATE INDEX kisses_kissedBy on kisses(kissedBy)', function () { cb(); });
+  },
+  function () {
+    db.close();
+  }
+]);
