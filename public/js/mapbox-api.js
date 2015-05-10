ad916dca94205b9df30d32f1cebe0033b6278f47
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-05-10 18:57:00 +0300

    Integrated mapbox and removed Yandex Maps

---
+++ public/js/mapbox-api.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  var CIRCLE_RADIUS = 50;
+
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYXJ0aWUxOCIsImEiOiJKLVRmRUVrIn0.D55B8_EOLw9r7HQo-M6zMQ';
+  var map = L.mapbox.map('map', 'artie18.m559o3ke');
+
+  // Move to backend-api.js after RequireJS is added
+  $.ajax({
+    url: "/api/markers/all",
+    type: "GET",
+    success: function(_response) {
+      var data = _response.res,
+          mbOptions = { color: '#FF0055',
+                        stroke: false };
+
+      for(var i = 0; i < data.length; i++) {
+        var elem = data[i],
+            position = [elem.lt, elem.ln];
+
+        L.circle(position, CIRCLE_RADIUS, mbOptions).addTo(map);
+      }
+
+    }
+  });
+
+
+});
