2a6ba4b041048f01b6e6a1c52264475fe360526e
Artyom Fedenka <artyom.fedenka@transferwise.com>
2015-10-09 18:17:36 +0300

    Updated the initial design --work-in-progress--

--- public/js/mapbox-api.js
+++ public/js/mapbox-api.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   var CIRCLE_RADIUS = 50;
 
   L.mapbox.accessToken = 'pk.eyJ1IjoiYXJ0aWUxOCIsImEiOiJKLVRmRUVrIn0.D55B8_EOLw9r7HQo-M6zMQ';
-  var map = L.mapbox.map('map', 'artie18.m559o3ke');
+  var map = L.mapbox.map('map', 'artie18.m559o3ke', { zoomControl: false });
+  new L.Control.Zoom({ position: 'topright' }).addTo(map);
 
   // Move to backend-api.js after RequireJS is added
   $.ajax({
